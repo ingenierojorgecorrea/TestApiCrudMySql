1937c6f8c56f5dc7bdb840c098008f5354dab6fa
Jorge Correa <32176868+ingenierojorgecorrea@users.noreply.github.com>
2022-02-27 22:07:51 -0500

    Delete DDL folder

--- DDL/2_StoreProcedure_JorgeCorrea.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE `GetTotalEmployeesByDepartment` ()
-
-	SELECT D.Name, COUNT(E.DepartmentId) AS Total
-	FROM Department D
-	LEFT JOIN Employee E
-	ON D.Id = E.DepartmentId
-	GROUP BY D.Name
-	ORDER BY Total DESC, Name ASC;
